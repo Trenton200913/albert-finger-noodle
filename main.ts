caa157353bf2dd8a46857a706c2cc96b15b316d4
Trenton200913 <111209161+Trenton200913@users.noreply.github.com>
2023-11-02 13:54:53 -0500

    Update pxt.json, main.blocks, main.ts, Albert-finger-noodles-instructions.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     info.changeScoreBy(1)
 })
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.splash("https://youtu.be/34Ig3X59_qA?si=lvuLM_vS3z38-GCh")
+})
 let albert_finger_noodle = sprites.create(assets.image`Albert-Finger-Noodle`, SpriteKind.Player)
-controller.moveSprite(albert_finger_noodle)
+controller.moveSprite(albert_finger_noodle, -100, -100)
 tiles.setCurrentTilemap(tilemap`level`)
 scene.cameraFollowSprite(albert_finger_noodle)
